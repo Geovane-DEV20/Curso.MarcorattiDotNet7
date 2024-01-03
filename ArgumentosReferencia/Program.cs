d476cbc8e78da047825d8fdea8aa56111d4790b6
geovane <geova@DESKTOP-PPJUNLP>
2024-01-03 20:29:05 -0300

    passando argumentos por referência

---
+++ ArgumentosReferencia/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Passando argumentos por referencia");
+
+int x = 20;
+Console.WriteLine("\n Valor do argumento x antes de passar por valor" + x);
+
+Calculo calc = new();
+calc.Dobrar(ref x);
+
+Console.WriteLine("\n Vaor do argumento x depois de passar por valor " + x);
+
+Console.ReadKey();
+
+public class Calculo
+{
+    public void Dobrar (ref int y) 
+    { 
+    
+        y *= 2;
+        Console.WriteLine("\n Valor do parametro y no método Dobrar " + y  );
+
+    }   
+}
