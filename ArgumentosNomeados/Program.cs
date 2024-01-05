af26dfdced4f4a324345e4cac284ce1bb5fdbb6b
geovane <geova@DESKTOP-PPJUNLP>
2024-01-05 20:51:52 -0300

    Argumentos Nomeados

---
+++ ArgumentosNomeados/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Email email = new Email();
+email.Enviar(destino: "geovane@teste.com", titulo: "teste", assunto: "teste do teste");
+
+public class Email
+{
+    public void Enviar(string destino, string titulo, string assunto)
+    {
+        Console.WriteLine($"{destino}, {titulo}:{assunto}");
+        
+    }
+}
