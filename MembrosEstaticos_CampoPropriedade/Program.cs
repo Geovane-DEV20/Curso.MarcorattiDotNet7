dd9524b5c99a1639a7c8873193149cd99eccfe92
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-01-09 01:25:09 -0300

    Membros Estaticos

---
+++ MembrosEstaticos_CampoPropriedade/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var teste = Cliente.Nome;
+teste = "Geovane";
+
+public class Cliente
+{
+    public static string? Nome;
+    public static int Idade;
+
+
+    public Cliente(string? nome, int idade) 
+    { 
+        Nome = nome;
+        Idade = idade;  
+    }
+}
---
+++ MetodosEstaticos/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Os metodos estaticos nao dependem de instancia de um objeto
+// Pertecem a classe e nao objeto
+var soma = Calculadora.Somar(2, 2);
+
+Console.ReadKey();
+
+public class Calculadora
+{
+    public static int Somar(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+    public static int Subtrair(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+
+    public static int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+    public static int Dividir(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+
+}
