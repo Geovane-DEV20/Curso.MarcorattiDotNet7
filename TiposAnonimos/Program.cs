602e50e8f5d103bca3b0556120fb7f3ba3e59e91
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-01-11 17:41:59 -0300

    Struct, TiposAnonimos, TryCath

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var aluno = new
+{
+    Nome = "Maria",
+    Idade = 42
+};
+
+
+Console.WriteLine(aluno.Nome + " - " + aluno.Idade + "anos");
+
---
+++ TryCath/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Informe o valor x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+try { 
+
+    int z = x / y;
+    Console.WriteLine($"{x} / {y} = {z}");
+
+}
+catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex?.StackTrace?.ToString());
+
+}
+finally {
+    Console.WriteLine("Processamento concluido");
+        
+        
+        }
+
+
+Console.ReadKey(); 
+
