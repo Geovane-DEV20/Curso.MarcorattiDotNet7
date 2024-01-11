c929d6a93831b5dd520a4e24d517eba7bd2b3603
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-01-11 00:15:44 -0300

    Enum e Struct

---
+++ Enum/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int dia = (int)DiasSemana.Quarta;
+Console.WriteLine(dia);
+enum DiasSemana
+{
+    Segunda, //1
+    Terça, //2
+    Quarta, //3
+    Quinta, //4
+    Sexta, //5
+    Sabado = 6,
+    Domingo
+}
+
+//Um enum pode ser qualquer tipo de dado numerico
