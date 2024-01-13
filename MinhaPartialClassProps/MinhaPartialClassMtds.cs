e223ab2a47453a697de09bd5d3b3fe1811f157b9
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-01-13 00:07:08 -0300

    Partial Class

---
+++ MinhaPartialClassProps/MinhaPartialClassMtds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public partial class MinhaPartialClass
+{
+    public TimeSpan CalulaIdade(DateTime DataNascimento)
+    {
+        return (DateTime.Now.Date - DataNascimento);
+    }
+
+    public TimeSpan DiferencaEntreDatas(DateTime data1, DateTime data2)
+    {
+        var diferenca = data1.Subtract(data2);
+        return diferenca;
+    }
+}
+
---
+++ MinhaPartialClassProps/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+
+MinhaPartialClass partialCLass = new MinhaPartialClass();   // Eu consigo usar metodos e propriedades de diferentes classes
