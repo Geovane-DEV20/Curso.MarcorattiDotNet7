d810ce81124bff5154d9f40488dde5a4a521efdc
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-01-16 01:32:11 -0300

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int[] numeros;
+numeros = new int[10] {1,2,3,4,5,6,7,8,9,10}; //mesma coisa eu consigo fazer para o tipo string
+
+Console.WriteLine(numeros[2]);
+
+numeros[0] = 9; //atribuindo valores
