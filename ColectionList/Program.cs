7b03b59c5fdcdf9a10c871b9436eb55b0fceb4ae
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-02-12 16:03:28 -0300

    POO

---
+++ ColectionList/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//declarando uma coleção do tipo List<t> e criando uma variavel
+var lista = new List<string>()
+{
+    "Geovane", "Fiego", "Hamilton"
+};
+
+
+lista.Insert(2, "bia");
+
+string[] array1 = { "Dina", "Carlos" };
+lista.AddRange(array1);
+lista.InsertRange(1, array1);
+
+
+for  (int i = 0; i < lista.Count; i++)
+{
+    Console.WriteLine( $"{lista[i]}");
+}   
---
+++ Heranca_ConceitoSintaxe/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Funcionario funcionario = new Funcionario();
+funcionario.Nome = "Josee";
+funcionario.Email = "josee@gmail.com";
+
+Console.WriteLine(funcionario.Nome);
+Console.WriteLine(funcionario.Email);
+
+
+Aluno aluno = new Aluno();
+aluno.Nota = 10;
+aluno.Nome = "Dieguinho";
+aluno.Email = "Dieguinho@Hotmail.com";
+aluno.Identificar();
+public class Pessoa
+{
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+
+    public void Identificar()
+    {
+        Console.WriteLine($"{Nome} - {Email}");
+    }
+}
+
+public class Funcionario : Pessoa
+{
+    public string? Empresa { get; set; }
+
+    public decimal Salario { get; set; }
+}
+
+public class Aluno : Pessoa
+{
+    public string? Curso { get; set; }
+    public int Nota { get; set; }
+}
