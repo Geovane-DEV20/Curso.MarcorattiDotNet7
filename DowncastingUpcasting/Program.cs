ea3763b00137ca9cda9d204e8eaa582a85023a1f
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-02-16 10:43:28 -0300

    Downcasting and Upcasting

---
+++ DowncastingUpcasting/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Upcasting 
+
+Forma circulo = new Circulo(10, 20);
+circulo.Desenhar(); // upcasting -> implicita
+
+Circulo circuloOne = new Circulo();
+Forma forma = circuloOne;
+Circulo c = (Circulo)forma; //dowcasting -> explicita
+c.PintarCirculo();  
+c.Desenhar();  
+public class Forma
+{
+    protected int xpos, ypos;
+    public Forma()
+    {
+            
+    }
+    public Forma(int x, int y)
+    {
+
+        xpos = x;   
+        ypos = y;   
+    }
+
+    public virtual void Desenhar()
+    {
+        Console.WriteLine("Desenhar - Forma");
+    }
+
+}
+
+public class Circulo : Forma
+{
+    public Circulo()
+    {
+            
+    }
+    public Circulo(int x, int y) : base(x, y) { }
+
+    public override void Desenhar()
+    {
+        Console.WriteLine("Desenhar - Circulo");
+    }
+
+    public void PintarCirculo()
+    {
+        Console.WriteLine("Pintar o circulo");
+    }
+
+}
