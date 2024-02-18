e7d8a11b9cc0c45bc4edcf457331c8b8c589bad0
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-02-17 23:45:59 -0300

    Modificador Abstrato

---
+++ ModificadorAbstract/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+// A Implementação do método abstrato é feita na classe derivada
+// Abstract nao pode ser instanciada
+public abstract class Forma
+{
+
+    public string Cor { get; set; }
+    public double Area { get; set; }
+    public double Perimetro { get; set; }
+
+    public abstract void CalcularArea();
+    public abstract void CalcularPerimetro();
+
+    public string Descrição()
+    {
+        return "Método comum da classe Forma";
+    }
+    
+}
+
+
+public class Quadrado : Forma
+{
+    public double Lado { get; set; }
+
+    public override void CalcularArea()
+    {
+        this.Area = Lado * Lado;        
+    }
+    public override void CalcularPerimetro()
+    {
+        this.Perimetro = 4 * Lado;
+    }
+
+}
