48517a495a15ae60c2786f7a2b3066fa23371f22
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-02-23 23:55:57 -0300

    Colecoes Nao Genericas e Genericas

---
+++ ColecoesNaoGenericasAndGenericas/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Coleções Nao Genericas e Genericas
+//Coleções Nao Genericas armazenam os elementos em arrays de objetos permitindo que eles contenham qualquer tipo de dados
+
+
+// As classes das coleções não genericas armazenam objetos dados do tipo object
+// Fazem a conversão implicita dos tipos de dados para object
+
+// As coleções genericas foram introduzidas na versão 2.0 da linguagem c# e estão presentes no namespace System.Collection.Generic sendo coleções fortemente
+// (Continuação)tipadas.
+// A RECOMENDAÇÃO É SEMPRE USAR AS COLEÇÕES GENERICAS
+
+
+
