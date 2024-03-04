daa5a54c407345ad10e6e39f476d3dc0645722d6
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-03-03 23:35:14 -0300

    SortedSet and HashSet

---
+++ HashSet/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+//HashSet nao permite a inclusao de elementos duplicados
+
+var timesSp = new HashSet<string>() {"Santos", "Palmeiras", "Sao Paul" };
+var timesRj = new HashSet<string>() {"Vasco", "Flamengo", "Fluminense" };
+var timesBa = new HashSet<string>() {"Bahia", "Vitoria", "Juazeiro" };
+
+var timeMundias = new HashSet<string>() { "Santos", "Palmeiras", "Sao Paulo", "Flamengo" };
+
+if (!timesSp.Contains("Corintias"))
+    timesSp.Add("Corintias");
+    timesSp.Add("Santos");
+
+var resultado = timesBa.Remove("Bahia");
+
+Console.ReadKey();  
---
+++ SortedSet/Program.cs
@@ -0,0 +1,16 @@
+﻿//ira ordenar os numeros
+
+var numeros = new List<int>() { 1,7,8,6,4,2};  
+
+SortedSet<int> conjuntos = new SortedSet<int>(numeros);
+
+
+foreach (int numero in conjuntos)
+{
+    Console.WriteLine(numero);
+}
+
+
+
+
+Console.ReadKey();
