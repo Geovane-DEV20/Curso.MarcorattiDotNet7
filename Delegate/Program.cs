0f691155518793fc12ad279a33f85e33615e9667
Geovane Donizete Laera <g.donizete.laera@avanade.com>
2024-03-05 23:56:04 -0300

    Delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿//é um tipo que representa referencias a mpetodos com uma lista  de parametros
+// delegates sao usados para implementar eventos
+// 
+
+
+DelegateCalculadora calcu = new DelegateCalculadora(Calculdadora.Somar);
+var resultado = calcu.Invoke(20, 20);
+
+
+Console.ReadKey();
+
+
+public delegate int DelegateCalculadora(int x, int y);
+
+public class Calculdadora { 
+    public  static int Somar(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+    public int Subtrair(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+}
+
+
